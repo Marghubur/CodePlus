f6dde2ba2deee80a4f2331732ae78810a6198620
Marghub <marghub12@gmail.com>
2023-10-20 09:14:23 +0530

    end point changed

--- src/app/services/ajax.service.ts
+++ src/app/services/ajax.service.ts
@@ -5,10 +5,10 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AjaxService {
-  baseUrl: string = "http://localhost:5000/api/";
-  // baseUrl: string = "https://marghub.bsite.net/api/";
-  imgBaseUrl: string = "http://localhost:5000/";
-  //imgBaseUrl: string = "https://marghub.bsite.net/";
+  //baseUrl: string = "http://localhost:5000/api/";
+   baseUrl: string = "https://marghub.bsite.net/api/";
+  //imgBaseUrl: string = "http://localhost:5000/";
+  imgBaseUrl: string = "https://marghub.bsite.net/";
 
   constructor(private http: HttpClient) { }
 
