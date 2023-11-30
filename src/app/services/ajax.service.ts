db557f61bf589dd35637a798ebdf3b0d3f81e955
Marghub <marghub12@gmail.com>
2023-11-30 10:18:21 +0530

    somee

--- src/app/services/ajax.service.ts
+++ src/app/services/ajax.service.ts
@@ -6,11 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class AjaxService {
   //baseUrl: string = "http://localhost:7500/api/";
-  //baseUrl: string = "//codeplus.somee.com/api/";
-  baseUrl: string = "https://marghub.bsite.net/api/";
+  baseUrl: string = "//codeplus.somee.com/api/";
+  //baseUrl: string = "https://marghub.bsite.net/api/";
   // imgBaseUrl: string = "http://localhost:7500/";
-  //imgBaseUrl: string = "//codeplus.somee.com/";
-  imgBaseUrl: string = "https://marghub.bsite.net/";
+  imgBaseUrl: string = "//codeplus.somee.com/";
+  //imgBaseUrl: string = "https://marghub.bsite.net/";
   
   constructor(private http: HttpClient) { }
 
